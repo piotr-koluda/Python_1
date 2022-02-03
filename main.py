be030178a3650c112c47ffe1d1ba5e95b8da8da6
PiotrKoluda <piotrkoluda@gmail.com>
2022-02-03 14:23:17 +0100

    adding new template

--- main.py
+++ main.py
@@ -2,11 +2,16 @@
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# section to import library. It could be created by myself or downloaded.
+import Training
+
+
 
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
 
 def print_twinkle(name):
     print('Twinkle, twinkle, little star,\n\tHow I wonder what you are! ')
@@ -14,7 +19,8 @@ def print_twinkle(name):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_twinkle('test')
-
-
-
+    a = [1,1,2,3,5,8,13,21,34,55,89]
+    Training.odd_even(3)
+    Training.list_les(a, 55)
+    Training.divisorts(12)
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
